803f080cf09b944de3942092772ef28b9e8d104d
jingyang <tom@kliyes.com>
2017-05-05 12:13:17 +0800

    use i18n patterns

--- src/urls.py
+++ src/urls.py
@@ -12,10 +12,30 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+
+Use Django Translation and Internationalization features, see:
+    https://docs.djangoproject.com/en/1.11/topics/i18n/translation/#how-django-discovers-language-preference
+    https://docs.djangoproject.com/en/1.11/topics/i18n/translation/#language-prefix-in-url-patterns
+Examples:
+prefix_default_language == True
+    http://example.com/en/admin/, will display English admin site
+    http://example.com/zh-hans/admin/, will display Chinese admin site
+prefix_default_language == False and settings.LANGUAGE_CODE == "en":
+    http://example.com/admin/, will display English admin site
+    http://example.com/en/admin/, will raise 404
 """
 from django.conf.urls import url
+from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
-urlpatterns = [
+
+# django translation
+# https://docs.djangoproject.com/en/1.11/topics/i18n/translation/#how-django-discovers-language-preference
+# https://docs.djangoproject.com/en/1.11/topics/i18n/translation/#language-prefix-in-url-patterns
+# for example:
+#
+urlpatterns = i18n_patterns(
     url(r'^admin/', admin.site.urls),
-]
+
+    prefix_default_language=False
+)
